6527123c0a5ebbb06078aa675eefd9c92fd3549d
Vitor Arruda <vitor_pimenta20@hotmail.com>
2018-03-10 19:31:13 -0300

    Termina guiengine (versão 1)
    Quase termina testes (MouseAware não tem testes automatizados)
    
    Implementa amostra de funcionamento da engine

--- guiengine.py
+++ guiengine.py
@@ -1,3 +1,6 @@
+import time
+from enum import Enum
+
 import pygame
 
 
@@ -48,6 +51,9 @@ class BusProxy:
     def __init__(self):
         self._listeners = {}
         self._proxied_bus = EventBus.active()
+        if self._proxied_bus is None:
+            raise RuntimeError('No bus exists. Are you requesting ' +
+                               'a BusProxy outside the scene environment ?')
 
     def __ensure_exists(self, event_name):
         if self._listeners.get(event_name) is None:
@@ -79,11 +85,111 @@ class BusProxy:
         self._listeners.clear()
 
     def emit(self, event_name, event_data):
-        # Ops, should only delegate
-        # self.__ensure_exists(event_name)
-        # for callback in self._listeners[event_name]:
-        #     callback(event_data)
+        # Só redireciona ao bus original
         self._proxied_bus.emit(event_name, event_data)
+
+
+class OuterBus(EventBus):
+
+    def __init__(self):
+        super().__init__()
+        self.__redirection_target = None
+
+    def redirect(self, target_bus):
+        self.__redirection_target = target_bus
+
+    def emit(self, event_name, event_data):
+        if self.__redirection_target is not None:
+            self.__redirection_target.emit(event_name, event_data)
+        else:
+            super().emit(event_name, event_data)
+
+    def refresh(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.emit(Event.QUIT, None)
+            elif event.type == pygame.MOUSEMOTION:
+                self.emit(Event.MOUSEMOVE, event.pos)
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                self.emit(Event.MOUSEDOWN, event.pos)
+            # elif second button or middle button, emit other events
+            elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                self.emit(Event.MOUSEUP, event.pos)
+            # Teclas, por enquanto, não são necessárias
+
+
+class Event(Enum):
+    QUIT = 'quit'
+    MOUSEMOVE = 'mousemove'
+    MOUSEDOWN = 'mousedown'
+    MOUSEUP = 'mouseup'
+    SCENE_CHANGE = 'scene-change'
+
+
+class MouseAware:
+
+    def __init__(self, bounds_lambda):
+        self.bounds_lambda = bounds_lambda
+        self.__mousedown_at = None
+        self.__mouseinside = False
+        self.__isdragging = False
+
+    def __onmousedown(self, data):
+        self.__mousedown_at = data
+
+    def __onmouseup(self, data):
+        if self.__mousedown_at is None:
+            return
+        # click
+        if abs(data[0] + data[1] - (self.__mousedown_at[0] + self.__mousedown_at[1])) < 10 \
+                and self.bounds_lambda().collidepoint(self.__mousedown_at):
+            self.onclick()
+            self.__mousedown_at = None
+        # dragend
+        if self.__isdragging is True:
+            self.ondragend(data)
+            self.__isdragging = False
+
+    def __onmousemove(self, data):
+        # mouseenter e mouseleave
+        # (talvez tenha que restringir a somente enquanto não estiver acontecendo um drag)
+        mouseinside = bool(self.bounds_lambda().collidepoint(data))
+        if mouseinside is True and self.__mouseinside is False:
+            self.onmouseenter()
+        elif mouseinside is False and self.__mouseinside is True:
+            self.onmouseleave()
+        self.__mouseinside = mouseinside
+        # dragstart
+        if self.__mousedown_at is not None and self.__isdragging is False:
+            was_inside = bool(self.bounds_lambda().collidepoint(self.__mousedown_at))
+            if was_inside:
+                self.ondragstart(self.__mousedown_at)
+                self.__isdragging = True
+        if self.__mousedown_at is not None and self.__isdragging is True:
+            self.ondrag(data)
+
+    def watch(self, bus):
+        bus.on(Event.MOUSEDOWN, self.__onmousedown)
+        bus.on(Event.MOUSEUP, self.__onmouseup)
+        bus.on(Event.MOUSEMOVE, self.__onmousemove)
+
+    def onclick(self):
+        pass
+
+    def onmouseenter(self):
+        pass
+
+    def onmouseleave(self):
+        pass
+
+    def ondragstart(self, pos):
+        pass
+
+    def ondrag(self, pos):
+        pass
+
+    def ondragend(self, pos):
+        pass
 
 
 class Rect(pygame.Rect):
@@ -110,6 +216,20 @@ class ResourceBank:
         if self.__bank.get(path) is None:
             self.__bank[path] = pygame.image.load(path).convert_alpha()
         return self.__bank[path] if cached else pygame.image.load(path).convert_alpha()
+
+    def sound(self, path, cached=True):
+        if self.__bank.get(path) is None:
+            self.__bank[path] = pygame.mixer.Sound(path)
+        return self.__bank[path] if cached else pygame.mixer.Sound(path)
+
+    def __localfont(self, fontname, size):
+        return str(fontname) + ':size:' + str(size)
+
+    def font(self, fontname, size, cached=True):
+        if self.__bank.get(self.__localfont(fontname, size)) is None:
+            self.__bank[self.__localfont(fontname, size)] = pygame.font.Font(fontname, size)
+        return self.__bank[self.__localfont(fontname, size)] if cached \
+            else pygame.font.Font(fontname, size)
 
 
 class Image:
@@ -142,14 +262,71 @@ class Image:
         return self.__surf
 
 
+class Text:
+
+    def __init__(self, content, size, font, color):
+        self.__content = content
+        self.__color = color
+        self.__font = pygame.font.Font(font, size)
+        self.__dirty = True
+        self.__surf = None
+
+    def color(self, color_tuple):
+        if self.__color != color_tuple:
+            self.__color = color_tuple
+            self.__dirty = True
+
+    def content(self, string=None):
+        if string is None:
+            return self.__content
+        if self.__content != string:
+            self.__content = string
+            self.__dirty = True
+
+    def to_surface(self):
+        if self.__dirty:
+            self.__surf = self.__font.render(
+                self.__content, True, self.__color)
+            self.__dirty = False
+        return self.__surf
+
+
+class Sound:
+
+    def __init__(self, path):
+        self.__sound = ResourceBank.instance().sound(path)
+        self.__channel = None
+
+    def play(self, repeat=0):
+        if self.__channel is not None:
+            self.__channel.play(self.__sound, repeat)
+        else:
+            self.__channel = self.__sound.play(repeat)
+        return self
+
+    def stop(self):
+        if self.__channel is not None:
+            self.__channel.stop()
+        return self
+
+
+class EmptySound:
+
+    def play(self, repeat=0):
+        return self
+
+    def stop(self):
+        return self
+
+
 class DrawContext:
 
     def __init__(self, root_context, origin):
         self.__root = root_context
         self.__origin = origin
 
-    def blit(self, image, xy: tuple):
-        return self.__root.blit(image,
+    def blit(self, imagelike, xy: tuple):
+        return self.__root.blit(imagelike,
                                 tuple(l1 + l2 for l1, l2 in zip(xy, self.__origin)))
 
     def line(self, xy1, xy2):
@@ -174,25 +351,198 @@ class DrawContext:
         return DrawContext(self.__root,
                            tuple(l1 + l2 for l1, l2 in zip(self.__origin, origin)))
 
+    def fill(self, color):
+        pass
+
 
 class RootDrawContext(DrawContext):
 
-    def __init__(self, origin, surface):
+    def __init__(self, surface):
         # A origem de um DrawContext é relativa ao RootDrawContext (por isso o (0,0))
         super().__init__(self, (0, 0))
-        self.__origin = origin
         self.__surface = surface
 
-    def blit(self, image, xy):
-        return self.__surface.blit(image.to_surface(),
-                                   tuple(l1 + l2 for l1, l2 in zip(self.__origin, xy)))
+    def blit(self, imagelike, xy):
+        return self.__surface.blit(imagelike.to_surface(), xy)
+
+    def fill(self, color):
+        self.__surface.fill(color)
+
+
+class Display:
+
+    def __init__(self, width, height):
+        self.__ctx = None
+        pygame.init()
+        self.resolution(width, height)
+
+    def resolution(self, width, height):
+        pygame.display.set_mode((width, height))
+        self.__ctx = RootDrawContext(pygame.display.get_surface())
+
+    def draw_context(self):
+        return self.__ctx
+
+    def flip(self):
+        pygame.display.flip()
 
 
 class Renderizable:
 
-    def __init__(self):
-        self.bounds = Rect((0, 0), (0, 0))
+    def __init__(self, xy):
+        self.bounds = Rect(xy, (0, 0))
+        self._bus = BusProxy()
+        self.__xy = xy
 
-    def update_render(self):
-        # IMPLEMENTAR !
+    def xy(self, point=None):
+        if point is None:
+            return self.__xy
+        self.__xy = point
+
+    def update_render(self, draw_context: DrawContext, dt):
         pass
+
+    def update_logic(self, dt):
+        pass
+
+    def destroy(self):
+        self._bus.disable_all()
+
+
+class FigureNode(Renderizable):
+
+    def __init__(self, xy, image):
+        super().__init__(xy)
+        self.__image = image
+        # Não adianta fazer isso, porque a origem continuará relativa, não absoluta
+        # self.bounds = image.get_rect()
+
+    def set_image(self, image):
+        self.__image = image
+
+    def update_render(self, draw_context: DrawContext, dt):
+        self.bounds = draw_context.blit(self.__image, self.xy())
+
+
+class TextNode(Renderizable):
+
+    def __init__(self, xy, text):
+        super().__init__(xy)
+        self.__text = text
+
+    def update_render(self, draw_context: DrawContext, dt):
+        draw_context.blit(self.__text, self.xy())
+
+
+class Layer(Renderizable):
+
+    def __init__(self, xy):
+        super().__init__(xy)
+        self.__children = []
+
+    def _add_child(self, renderizable):
+        self.__children.append(renderizable)
+
+    def _remove_child(self, renderizable):
+        self.__children.remove(renderizable)
+
+    def update_render(self, draw_context: DrawContext, dt):
+        for child in self.__children:
+            # self.xy() é relativo ao draw_context fornecido a esta Layer, então OK
+            child.update_render(draw_context.sub(self.xy()), dt)
+        if len(self.__children) == 0:
+            self.bounds = Rect(self.xy(), (0, 0))
+        else:
+            self.bounds = self.__children[0].bounds.unionall(
+                [x.bounds for x in self.__children])
+
+    def update_logic(self, dt):
+        for child in self.__children:
+            child.update_logic(dt)
+
+    def destroy(self):
+        for child in self.__children:
+            child.destroy()
+        self.__children.clear()
+
+
+class Scene(Layer):
+
+    def __init__(self):
+        super().__init__((0, 0))
+        self.__bgm = EmptySound()
+        self._parts()
+
+    def update_render(self, draw_context: DrawContext, dt):
+        draw_context.fill((0, 0, 0))
+        super().update_render(draw_context, dt)
+
+    def _bgm(self, sound: Sound):
+        self.__bgm.stop()
+        self.__bgm = sound
+        self.__bgm.play(-1)
+
+    def destroy(self):
+        super().destroy()
+        self.__bgm.stop()
+
+    def _parts(self):
+        """
+        Classes filhas devem se construir aqui (devem chamar _add_child() etc aqui)
+        :return: Qualquer coisa, porque o retorno será desprezado
+        """
+        pass
+
+
+class SceneManager:
+
+    def __init__(self, draw_context, bus, initial_scene):
+        self.__ctx = draw_context
+        self.__bus = bus
+        self.__requested_scene = None
+        self.__current = initial_scene()
+        self.__bus.on(Event.SCENE_CHANGE, self.__event_listener)
+
+    def __event_listener(self, data):
+        self.__requested_scene = data
+
+    def __check_change_scene(self):
+        if self.__requested_scene is not None:
+            self.__current.destroy()
+            self.__current = self.__requested_scene()
+            self.__requested_scene = None
+
+    def tick(self, dt):
+        self.__check_change_scene()
+        self.__current.update_logic(dt)
+        self.__current.update_render(self.__ctx, dt)
+
+
+class GameObject:
+
+    def __init__(self, display, initial_scene):
+        self.__display = display
+        self.__outer_bus = OuterBus()
+        self.__bus = EventBus()
+        EventBus.active(self.__bus)
+        self.__bus.on(Event.QUIT, self.__quit_listener)
+        self.__outer_bus.redirect(self.__bus)
+        self.__scene_mgr = SceneManager(display.draw_context(), self.__bus, initial_scene)
+        self.__should_stop = False
+
+    def __quit_listener(self, data):
+        self.__should_stop = True
+
+    def gameloop(self):
+        time_now = time.clock()
+        while not self.__should_stop:
+            self.__outer_bus.refresh()
+            time_before = time_now
+            time_now = time.clock()
+            dt = time_now - time_before
+            if dt < 1 / 60:
+                time.sleep(1 / 60 - dt)
+            if dt > 20 * 1 / 60:
+                dt = 1 / 60
+            self.__scene_mgr.tick(dt)
+            self.__display.flip()
--- guiengine_test.py
+++ guiengine_test.py
@@ -112,6 +112,82 @@ class TestBusProxy(unittest.TestCase):
         self.assertTrue(self.called.get(4))
 
 
+class TestOuterBus(unittest.TestCase):
+    class Listeners:
+
+        def __init__(self, outer):
+            """
+
+            :type outer TestOuterBus
+            """
+            self.outer = outer
+            self.moved = False
+            self.button_down = False
+            self.button_up = False
+
+        def onmousemove(self, data):
+            self.moved = True
+            self.outer.assertEqual(data, (50, 50))
+
+        def onbuttondown(self, data):
+            self.button_down = True
+            self.outer.assertEqual(data, (40, 60))
+
+        def onbuttonup(self, data):
+            self.button_up = True
+            self.outer.assertEqual(data, (40, 60))
+
+    def setUp(self):
+        self.outer_bus = OuterBus()
+        self.mousemove = pygame.event.Event(pygame.MOUSEMOTION, {
+            'pos': (50, 50),
+            'rel': (-10, 30),
+            'buttons': (False, False, False)
+        })
+        self.buttondown = pygame.event.Event(pygame.MOUSEBUTTONDOWN, {
+            'button': 1,
+            'pos': (40, 60)
+        })
+        self.buttonup = pygame.event.Event(pygame.MOUSEBUTTONUP, {
+            'button': 1,
+            'pos': (40, 60)
+        })
+        self.listeners = self.Listeners(self)
+
+    def _launch(self, listen_on_bus):
+        pygame.event.post(self.mousemove)
+        pygame.event.post(self.buttondown)
+        pygame.event.post(self.buttonup)
+        listen_on_bus.on(Event.MOUSEMOVE, self.listeners.onmousemove)
+        listen_on_bus.on(Event.MOUSEUP, self.listeners.onbuttonup)
+        listen_on_bus.on(Event.MOUSEDOWN, self.listeners.onbuttondown)
+
+    def test_emit_refresh(self):
+        self._launch(self.outer_bus)
+        self.outer_bus.refresh()
+        self.assertTrue(self.listeners.moved)
+        self.assertTrue(self.listeners.button_up)
+        self.assertTrue(self.listeners.button_down)
+
+    def test_redirect(self):
+        bus = EventBus()
+        self.outer_bus.redirect(bus)
+        self._launch(bus)
+        self.outer_bus.refresh()
+        self.assertTrue(self.listeners.moved)
+        self.assertTrue(self.listeners.button_up)
+        self.assertTrue(self.listeners.button_down)
+
+
+class TestMouseAware(unittest.TestCase):
+    """
+    TODO: Testar MouseAware... ou não...
+    """
+
+    def test_nothing(self):
+        pass
+
+
 class TestResourceBank(unittest.TestCase):
 
     def setUp(self):
@@ -125,6 +201,14 @@ class TestResourceBank(unittest.TestCase):
 
     def test_image(self):
         self.assertIsInstance(self.bank.image(self.paths[0]), pygame.Surface)
+
+    def test_sound(self):
+        sound = self.bank.sound('Music/Music.ogg')
+        self.assertIsInstance(sound, pygame.mixer.Sound)
+
+    def test_font(self):
+        font = self.bank.font(None, 12)
+        self.assertIsInstance(font, pygame.font.Font)
 
     def test_caching(self):
         self.assertEqual(self.bank.image(self.paths[0]), self.bank.image(self.paths[0]))
@@ -147,32 +231,290 @@ class TestImage(unittest.TestCase):
         self.assertEqual(new_rf, self.image)
 
 
+class TestText(unittest.TestCase):
+
+    def setUp(self):
+        self.c1 = 'Hola'
+        self.c2 = 'Adios'
+        self.txt = Text(self.c1, 12, None, (0, 0, 0))
+
+    def test_to_surface(self):
+        # Deve retornar a mesma várias vezes, a não ser quando conteúdo ou cor mudar
+        s1 = self.txt.to_surface()
+        s2 = self.txt.to_surface()
+        self.txt.content(self.c2)
+        s3 = self.txt.to_surface()
+        s4 = self.txt.to_surface()
+        self.txt.color((0, 0, 255))
+        s5 = self.txt.to_surface()
+        s6 = self.txt.to_surface()
+        self.assertIs(s1, s2)
+        self.assertIsNot(s1, s3)
+        self.assertIs(s3, s4)
+        self.assertIsNot(s3, s5)
+        self.assertIs(s5, s6)
+
+
 class TestRootDrawContext(unittest.TestCase):
 
     def setUp(self):
-        self.ctx = RootDrawContext((20, 10), Surface((500, 500)))
+        self.ctx = RootDrawContext(Surface((500, 500)))
         self.img = Image('resources/Cburnett V2 improved/PNGs/square brown dark_png.png') \
             .scale(1 / 10)
 
     def test_blit(self):
         rect = self.ctx.blit(self.img, (30, 30))
-        self.assertEqual(rect.x, 20 + 30)
-        self.assertEqual(rect.y, 10 + 30)
+        self.assertEqual(rect.x, 30)
+        self.assertEqual(rect.y, 30)
 
 
 class TestDrawContext(unittest.TestCase):
 
     def setUp(self):
         initpygame()
-        self.root = RootDrawContext((20, 10), Surface((500, 500)))
+        self.root = RootDrawContext(Surface((500, 500)))
         self.img = Image('resources/Cburnett V2 improved/PNGs/square brown dark_png.png') \
             .scale(1 / 10)
 
     def test_sub_blit(self):
         sub = self.root.sub((40, 40)).sub((60, 60))
         rect = sub.blit(self.img, (50, 50))
-        self.assertEqual(rect.x, 20 + 40 + 60 + 50)
-        self.assertEqual(rect.y, 10 + 40 + 60 + 50)
+        self.assertEqual(rect.x, 40 + 60 + 50)
+        self.assertEqual(rect.y, 40 + 60 + 50)
+
+
+class TestSound(unittest.TestCase):
+
+    def setUp(self):
+        self.sound = Sound('Music/Music.ogg')
+
+    def test_not_throws(self):
+        self.sound.play(-1).stop().play(0).play(0).play(3).stop()
+
+
+class TestEmptySound(unittest.TestCase):
+
+    def test_nothing(self):
+        EmptySound().play(-1).play(0).stop().play(2).play(3).stop()
+
+
+class TestRenderizable(unittest.TestCase):
+
+    def setUp(self):
+        self.ren = Renderizable((10, 20))
+
+    def test_bounds(self):
+        self.assertEqual(self.ren.bounds.x, 10)
+        self.assertEqual(self.ren.bounds.y, 20)
+        self.assertEqual(self.ren.bounds.width, 0)
+        self.assertEqual(self.ren.bounds.height, 0)
+
+    def test_bus(self):
+        self.assertIsInstance(self.ren._bus, BusProxy)
+
+
+class TestFigureNode(unittest.TestCase):
+    class MockDrawContext(DrawContext):
+
+        def __init__(self):
+            self.blitted = False
+
+        def blit(self, imagelike, xy):
+            if xy == (10, 20):
+                self.blitted = True
+
+    def setUp(self):
+        self.fig = FigureNode((10, 20), Image(
+            'resources/Cburnett V2 improved/PNGs/square brown dark_png.png'
+        ).scale(1 / 10))
+
+    def test_update_render(self):
+        mock = self.MockDrawContext()
+        self.fig.update_render(mock, 0.01)
+        self.assertTrue(mock.blitted)
+
+
+class TestLayer(unittest.TestCase):
+    class MockNode(Renderizable):
+
+        def __init__(self, bounds):
+            super().__init__(bounds.topleft)
+            self.logic = False
+            self.render = False
+            self.destroyed = False
+            self.bounds = bounds
+
+        def update_logic(self, dt):
+            self.logic = True
+
+        def update_render(self, draw_context: DrawContext, dt):
+            self.render = True
+
+        def destroy(self):
+            self.destroyed = True
+
+    def setUp(self):
+        self.layer = Layer((10, 10))
+        self.c1 = TestLayer.MockNode(Rect((10, 10), (30, 40)))
+        self.c2 = TestLayer.MockNode(Rect((20, 10), (30, 40)))
+        self.layer._add_child(self.c1)
+        self.layer._add_child(self.c2)
+
+    def test_update_logic(self):
+        self.layer.update_logic(0.01)
+        self.assertTrue(self.c1.logic)
+        self.assertTrue(self.c2.logic)
+
+    def test_update_render(self):
+        self.layer.update_render(RootDrawContext(Surface((10, 10))), 0.01)
+        self.assertTrue(self.c1.render)
+        self.assertTrue(self.c2.render)
+        self.assertEqual(self.layer.bounds, Rect((10, 10), (40, 40)))
+
+    def test_remove_child(self):
+        self.layer._remove_child(self.c2)
+        self.layer.update_logic(0.01)
+        self.assertTrue(self.c1.logic)
+        self.assertFalse(self.c2.logic)
+
+    def test_destroy(self):
+        self.layer.destroy()
+        self.assertTrue(self.c1.destroyed)
+        self.assertTrue(self.c2.destroyed)
+
+
+class TestScene(unittest.TestCase):
+
+    def setUp(self):
+        self.scene = Scene()
+
+    def test_bgm(self):
+        sound = Sound('Music/Music.ogg')
+        self.scene._bgm(sound)
+
+
+class TestSceneManager(unittest.TestCase):
+    class MockDrawContext(DrawContext):
+
+        def __init__(self):
+            pass
+
+        def sub(self, origin):
+            pass
+
+        def blit(self, imagelike, xy: tuple):
+            pass
+
+        def circle(self, center, radius):
+            pass
+
+        def line(self, xy1, xy2):
+            pass
+
+        def fill(self, color):
+            pass
+
+    class MockScene(Scene):
+
+        def __init__(self, outer):
+            super().__init__()
+            self.outer = outer
+
+        def update_logic(self, dt):
+            self.outer.logic += 1
+            # Mesmo com esse evento, update_render ainda será chamado
+            # porque a troca de cenas é "lazy" (só acontece quando dou tick)
+            self._bus.emit(Event.SCENE_CHANGE, lambda: self.outer.second_scene)
+
+        def update_render(self, draw_context: DrawContext, dt):
+            self.outer.render += 1
+
+        def destroy(self):
+            self.outer.destroyed = True
+
+    class SecondMockScene(Scene):
+
+        def __init__(self, outer):
+            self.outer = outer
+
+        def update_logic(self, dt):
+            self.outer.logic -= 1
+
+        def update_render(self, draw_context: DrawContext, dt):
+            self.outer.render -= 1
+
+    def setUp(self):
+        self.ctx = self.MockDrawContext()
+        self.bus = EventBus()
+        EventBus.active(self.bus)
+        self.scene = self.MockScene(self)
+        self.second_scene = self.SecondMockScene(self)
+        self.mgr = SceneManager(self.ctx, self.bus, lambda: self.scene)
+        self.logic = 0
+        self.render = 0
+        # Marca quando a MockScene é destruída, momento no qual
+        # a SecondMockScene deve substituí-la
+        self.destroyed = False
+
+    def test_tick(self):
+        self.mgr.tick(0.01)
+        self.assertEqual(self.logic, 1)
+        self.assertEqual(self.render, 1)
+        self.mgr.tick(0.01)
+        self.assertTrue(self.destroyed)
+        self.assertEqual(self.logic, 0)
+        self.assertEqual(self.render, 0)
+
+
+class TestGameObject(unittest.TestCase):
+    class MockScene(Scene):
+
+        def __init__(self):
+            super().__init__()
+            self.cycles = {
+                'logic': 0,
+                'render': 0
+            }
+
+        def update_render(self, draw_context: DrawContext, dt):
+            self.cycles['render'] += 1
+
+        def update_logic(self, dt):
+            self.cycles['logic'] += 1
+            if self.cycles['logic'] == 100:
+                self._bus.emit(Event.QUIT, None)
+
+    class MockDisplay(Display):
+
+        def __init__(self):
+            self.flipped = 0
+
+        def draw_context(self):
+            return TestSceneManager.MockDrawContext()
+
+        def resolution(self, width, height):
+            pass
+
+        def flip(self):
+            self.flipped += 1
+
+    def create_scene(self):
+        self.scene = self.MockScene()
+        return self.scene
+
+    def setUp(self):
+        self.display = self.MockDisplay()
+        # Atenção aqui ! Não posso instanciar uma Scene antes de GameObject,
+        # porque este define um bus, enquanto a outra pede um bus
+        self.game_object = GameObject(self.display, self.create_scene)
+
+    def test_gameloop(self):
+        self.game_object.gameloop()
+        self.assertEqual(self.scene.cycles, {
+            'logic': 100,
+            'render': 100
+        })
+        self.assertEqual(self.display.flipped, 100)
 
 
 if __name__ == '__main__':
